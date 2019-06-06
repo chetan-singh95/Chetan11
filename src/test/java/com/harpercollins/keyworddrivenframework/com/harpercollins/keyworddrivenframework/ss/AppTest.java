2974d3164a8d3d0522d18b022bd51a5f11eda472
chetan-singh95 <chetansingh.peterian@gmail.com>
2019-06-07 00:40:41 +0530

    New Chanfg

---
+++ src/test/java/com/harpercollins/keyworddrivenframework/com/harpercollins/keyworddrivenframework/ss/AppTest.java
@@ -0,0 +1,20 @@
+package com.harpercollins.keyworddrivenframework.com.harpercollins.keyworddrivenframework.ss;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
